3f81d136bf11b8dff9419178fd39ba88f8e64365
MHProDev <mhprodev2020@gmail.com>
2022-02-26 01:00:55 +0330

    for debug

--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
-from pathlib import Path
 from setuptools import setup
-import PyRoxy
 
-with Path(__file__).with_name('requirements.txt').open("r+") as f:
+with open('requirements.txt').open("r+") as f:
     required = f.read().splitlines()
 
 setup(
     name='PyRoxy',
-    version=PyRoxy.__version__,
+    version="1.0 BETA",
     packages=['PyRoxy', 'PyRoxy.GeoIP', 'PyRoxy.Tools', 'PyRoxy.Exceptions'],
     url='https://github.com/MHProDev/PyRoxy',
     license='',
-    author=PyRoxy.__auther__,
+    author="MH_ProDev",
     author_email='',
     install_requires=required,
     include_package_data=True,
