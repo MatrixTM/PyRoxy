1cb1b2da7c5f5843521801d386ab04c11286929e
MH_ProDev <63018646+MHProDev@users.noreply.github.com>
2022-02-26 12:22:25 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='PyRoxy',
-    version="1.0 BETA",
+    version="1.0-BETA",
     packages=['PyRoxy', 'PyRoxy.GeoIP', 'PyRoxy.Tools', 'PyRoxy.Exceptions'],
     url='https://github.com/MHProDev/PyRoxy',
     license='',
@@ -10,5 +10,6 @@ setup(
     author_email='',
     install_requires=["maxminddb>=2.2.0", "requests>=2.27.1", "yarl>=1.7.2", "pysocks>=1.7.1"],
     include_package_data=True,
+    package_data={'PyRoxy.GeoIP.Sqlite':['*.txt', "GeoLite2-Country.mmdb"]},
     description=''
 )
