f6aaea4c636c1f95b704477c4ffdd618b1b1761c
MH_ProDev <63018646+MHProDev@users.noreply.github.com>
2022-02-26 13:05:28 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="1.0b2",
     packages=['PyRoxy', 'PyRoxy.GeoIP', 'PyRoxy.Tools', 'PyRoxy.Exceptions'],
     url='https://github.com/MHProDev/PyRoxy',
-    license='',
+    license='MIT',
     author="MH_ProDev",
     author_email='',
     install_requires=["maxminddb>=2.2.0", "requests>=2.27.1", "yarl>=1.7.2", "pysocks>=1.7.1"],
