a4fa60ee447c2ea990b7fe9856edaa2d7711671b
MH_ProDev <63018646+MHProDev@users.noreply.github.com>
2022-02-26 13:25:43 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,7 @@ setup(
     url='https://github.com/MHProDev/PyRoxy',
     license='MIT',
     author="MH_ProDev",
-    author_email='',
     install_requires=["maxminddb>=2.2.0", "requests>=2.27.1", "yarl>=1.7.2", "pysocks>=1.7.1"],
     include_package_data=True,
-    description=''
+    package_data={'PyRoxy.GeoIP':['*.txt', "Sqlite/GeoLite2-Country.mmdb"], '': ["LICENSE"]}
 )
