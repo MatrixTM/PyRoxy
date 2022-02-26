068cf54f69fae4b08635e64410ca2085f1291cc2
MH_ProDev <63018646+MHProDev@users.noreply.github.com>
2022-02-26 13:21:54 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,5 @@ setup(
     author_email='',
     install_requires=["maxminddb>=2.2.0", "requests>=2.27.1", "yarl>=1.7.2", "pysocks>=1.7.1"],
     include_package_data=True,
-    package_data={'PyRoxy.GeoIP':['*.txt', "GeoLite2-Country.mmdb"], '': ["LICENSE"]},
     description=''
 )
